2fd20a27ba8727807df071ac8b2dc8f50d034e1d
Jemienachukwu <79016579+Jemienachukwu@users.noreply.github.com>
2025-03-01 22:03:40 +0100

    sidebar integration

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,14 @@
+import Sidebar from "@/components/Sidebar";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedin = { firstName: "Jemie", LastName: "Jeffery" };
   return (
-    <main>
-      sidebar
+    <main className="h-full w-full flex font-inter">
+      <Sidebar user={loggedin} />
       {children}
     </main>
   );
