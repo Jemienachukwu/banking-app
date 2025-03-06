02fced6ba2918d0d45f7641752430bbcf168f548
Jemienachukwu <79016579+Jemienachukwu@users.noreply.github.com>
2025-03-06 21:59:12 +0100

    implementation of sign-up authentification on appwrite

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -2,6 +2,7 @@
 
 // import { ID, Query } from "node-appwrite";
 // import { createAdminClient, createSessionClient } from "../appwrite";
+// import { parseStringify } from "../utils";
 // import { cookies } from "next/headers";
 // import { encryptId, extractCustomerIdFromUrl, parseStringify } from "../utils";
 // import { CountryCode, ProcessorTokenCreateRequest, ProcessorTokenCreateRequestProcessorEnum, Products } from "plaid";
@@ -295,7 +296,10 @@
 // }
 
 "use server";
-
+import { ID } from "node-appwrite";
+import { createAdminClient, createSessionClient } from "../appwrite";
+import { parseStringify } from "../utils";
+import { cookies } from "next/headers";
 export const signIn = async () => {
   try {
   } catch (error) {
@@ -303,8 +307,39 @@ export const signIn = async () => {
   }
 };
 export const signUp = async (userData: SignUpParams) => {
+  const { email, password, firstName, lastName } = userData;
   try {
+    const { account } = await createAdminClient();
+
+    const newUserAccount = await account.create(
+      ID.unique(),
+      email,
+      password,
+      `${firstName} ${lastName}`
+    );
+    const session = await account.createEmailPasswordSession(email, password);
+
+    (await cookies()).set("appwrite-session", session.secret, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "strict",
+      secure: true,
+    });
+    return parseStringify(newUserAccount);
   } catch (error) {
     console.error("Error", error);
   }
 };
+
+export async function getLoggedInUser() {
+  try {
+    const { account } = await createSessionClient();
+    const user = await account.get();
+
+    // const user = await getUserInfo({ userId: result.$id });
+    return parseStringify(user);
+  } catch (error) {
+    // console.log(error);
+    return null;
+  }
+}
