c536e64cd8c615f414eb7536bc9a9403b308b430
Jemienachukwu <79016579+Jemienachukwu@users.noreply.github.com>
2025-03-29 21:30:12 +0100

    get transaction history

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,36 +1,67 @@
 import HeaderBox from "@/components/HeaderBox";
+import RecentTransactions from "@/components/RecentTransactions";
 import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
+import { getAccount, getAccounts } from "@/lib/actions/bank.actions";
 import { getLoggedInUser } from "@/lib/actions/user.actions";
-import React from "react";
 
-const Home = async () => {
+export default async function Home({
+  searchParams: rawSearchParams,
+}: {
+  searchParams: Promise<{
+    id?: string;
+    page?: string;
+  }>;
+}) {
+  const searchParams = await rawSearchParams;
+
+  const { id: searchId, page: searchPage } = searchParams;
+  const id = searchId;
+  const page = searchPage;
+
   const loggedIn = await getLoggedInUser();
+  const accounts = await getAccounts({
+    userId: loggedIn.$id,
+  });
+
+  if (!accounts) return;
+
+  const accountsData = accounts?.data;
+  const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
+
+  const account = await getAccount({ appwriteItemId });
+
   return (
     <section className="home">
       <div className="home-content">
         <header className="home-header">
           <HeaderBox
             type="greeting"
-            title="welcome"
-            user={loggedIn?.name || "guest"}
-            subtext=" Access and manage your account and transactions efficiently."
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently."
           />
+
           <TotalBalanceBox
-            accounts={[]}
-            totalBanks={1}
-            totalCurrentBalance={1250.35}
+            accounts={accountsData}
+            totalBanks={accounts?.totalBanks}
+            totalCurrentBalance={accounts?.totalCurrentBalance}
           />
         </header>
-        RECENT TRANSACTIONS
+
+        <RecentTransactions
+          accounts={accountsData}
+          transactions={account?.transactions}
+          appwriteItemId={appwriteItemId}
+          page={1}
+        />
       </div>
+
       <RightSidebar
         user={loggedIn}
-        transactions={[]}
-        banks={[{ currentBalance: 133.67 }, { currentBalance: 893.67 }]}
+        transactions={account?.transactions}
+        banks={accountsData?.slice(0, 2)}
       />
     </section>
   );
-};
-
-export default Home;
+}
--- components/TransactionsTable.tsx
+++ components/TransactionsTable.tsx
@@ -1,6 +1,88 @@
+// import {
+//   formatAmount,
+//   formatDateTime,
+//   getTransactionStatus,
+//   removeSpecialCharacters,
+// } from "@/lib/utils";
+
+// const TransactionsTable = ({ transactions }: TransactionTableProps) => {
+//   // console.log("this is transactions tabel data", transactions);
+
+//   return (
+//     <table className="w-full border-collapse border border-gray-200">
+//       <thead className="bg-[#f9fafb]">
+//         <tr>
+//           <th className="px-2 border border-gray-200">Transaction</th>
+//           <th className="px-2 border border-gray-200">Amount</th>
+//           <th className="px-2 border border-gray-200">Status</th>
+//           <th className="px-2 border border-gray-200">Date</th>
+//           <th className="px-2 border border-gray-200 max-md:hidden">Channel</th>
+//           <th className="px-2 border border-gray-200 max-md:hidden">
+//             Category
+//           </th>
+//         </tr>
+//       </thead>
+//       <tbody>
+//         {transactions.map((t) => {
+//           const status = getTransactionStatus(new Date(t.date));
+//           const amount = formatAmount(t.amount);
+//           const isDebit = t.type === "debit";
+//           const isCredit = t.type === "credit";
+
+//           return (
+//             <tr
+//               key={t.id}
+//               className={`${
+//                 isDebit || amount[0] === "-" ? "bg-[#FFFBFA]" : "bg-[#F6FEF9]"
+//               } border-b border-gray-200`}
+//             >
+//               <td className="max-w-[250px] pl-2 pr-10 border border-gray-200">
+//                 <div className="flex items-center gap-3">
+//                   <h1 className="text-sm truncate font-semibold text-[#344054]">
+//                     {removeSpecialCharacters(t.name)}
+//                   </h1>
+//                 </div>
+//               </td>
+
+//               <td
+//                 className={`pl-2 pr-10 font-semibold border border-gray-200 ${
+//                   isDebit || amount[0] === "-"
+//                     ? "text-[#f04438]"
+//                     : "text-[#039855]"
+//                 }`}
+//               >
+//                 {isDebit ? `-${amount}` : isCredit ? amount : amount}
+//               </td>
+
+//               <td className="pl-2 pr-10 border border-gray-200">
+//                 {/* <CategoryBadge category={status} /> */}
+//                 {status}
+//               </td>
+
+//               <td className="min-w-32 pl-2 pr-10 border border-gray-200">
+//                 {formatDateTime(new Date(t.date)).dateTime}
+//               </td>
+
+//               <td className="pl-2 pr-10 capitalize min-w-24 border border-gray-200">
+//                 {t.paymentChannel}
+//               </td>
+
+//               <td className="pl-2 pr-10 max-md:hidden border border-gray-200">
+//                 {/* <CategoryBadge category={t.category} /> */}
+//                 {status}
+//               </td>
+//             </tr>
+//           );
+//         })}
+//       </tbody>
+//     </table>
+//   );
+// };
+
+// export default TransactionsTable;
 import React from "react";
 
-const TransactionsTable = () => {
+const TransactionsTable = ({ transactions }: TransactionTableProps) => {
   return <div>TransactionsTable</div>;
 };
 
